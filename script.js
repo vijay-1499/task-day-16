e1cf0efe99b81f3c6f920ace89283d657521b3d4
vijay <139314820+vijay-1499@users.noreply.github.com>
2023-10-16 21:36:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+document.getElementById('countdown').innerHTML = '10';
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '9';
+}, 1000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '8';
+}, 2000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '7';
+}, 3000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '6';
+}, 4000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '5';
+}, 5000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '4';
+}, 6000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '3';
+}, 7000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '2';
+}, 8000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '1';
+}, 9000);
+
+setTimeout(function() {
+    document.getElementById('countdown').innerHTML = 'Happy Independence Day';
+}, 10000);
